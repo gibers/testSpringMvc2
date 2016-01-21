5460aa3e361e4ab8007b3c5f8037caa222a21452
gibers <chbaptis@hotmail.fr>
2016-01-21 22:42:44 +0100

    first commit , example taken from SpringInAction Book

---
+++ deployerWar.bat
@@ -0,0 +1,10 @@
+
+
+rm C:\Users\Jean-Baptiste\Serveurs\apache-tomcat-7.0.55\webapps\testSpringMvc2.war
+
+rm C:\Users\Jean-Baptiste\Serveurs\apache-tomcat-7.0.55\webapps\testSpringMvc2
+
+cp target/*.war C:\Users\Jean-Baptiste\Serveurs\apache-tomcat-7.0.55\webapps\.
+
+
+pause
